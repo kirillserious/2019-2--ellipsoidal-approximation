3b1d06f26b53872a9e5aaedd1d0b2246bc8b050e
kirillserious <kireku@gmail.com>
2020-05-18 00:56:49 +0300

    Start report

--- source/index.m
+++ source/index.m
@@ -1,10 +1,34 @@
-%%
+%% Рисуем эллипс
 qVec = [1; 2];
-qMat = [1, 0; 0, 2];
+qMat = [5, 3; 3, 2];
 
 [xs, ys] = ellipse_coords(qVec, qMat);
-plot(xs, ys);
 
+figure, hold on, grid on;
+plot(xs, ys);
+xlabel('$$x_1$$', 'interpreter', 'latex');
+ylabel('$$x_2$$', 'interpreter', 'latex');
+legend off;
+%% Рисуем сумму двух эллипсов
+q1Vec = [0; 0];
+q2Vec = [0; 0];
+q1Mat = [1, 0; 0, 2];
+q2Mat = [5, 3; 3, 2];
+
+figure, hold on, grid on;
+color1 = [0, 0.4470, 0.7410];
+color2 = [0.8500, 0.3250, 0.0980];
+
+[xs, ys] = ellipse_coords(q1Vec, q1Mat);
+p1 = plot(xs, ys, 'Color', color1);
+[xs, ys] = ellipse_coords(q2Vec, q2Mat);
+plot(xs, ys, 'Color', color1);
+[xs, ys] = sum_ellipse_coords(q1Vec, q1Mat, q2Vec, q2Mat);
+p2 = plot(xs, ys, 'Color', color2);
+
+xlabel('$$x_1$$', 'interpreter', 'latex');
+ylabel('$$x_2$$', 'interpreter', 'latex');
+legend([p1, p2], 'Эллипсоиды', 'Сумма Минковского', 'Location', 'northwest');
 %%
 q1Vec = [1; 2];
 q2Vec = [2; 1];
@@ -33,18 +57,7 @@ for phi = phiVec
     [xs, ys] = outer_ellipse_coords(lVec, q1Vec, q1Mat, q2Vec, q2Mat);
     plot(xs, ys, 'b');
 end
-%%
-q1Vec = [0; 0];
-q2Vec = [0; 0];
-q1Mat = [1, 0; 0, 2];
-q2Mat = [5, 3; 3, 2];
-figure, hold on, grid on;
-[xs, ys] = ellipse_coords(q1Vec, q1Mat);
-plot(xs, ys, 'r');
-[xs, ys] = ellipse_coords(q2Vec, q2Mat);
-plot(xs, ys, 'r');
-[xs, ys] = sum_ellipse_coords(q1Vec, q1Mat, q2Vec, q2Mat);
-plot(xs, ys);
+
 %%
 q1Vec = [0; 0];
 q2Vec = [0; 0];
