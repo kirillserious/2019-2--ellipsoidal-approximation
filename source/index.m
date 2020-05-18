69cdc30ebdb248efef5c108146161edf69a40fe3
kirillserious <kireku@gmail.com>
2020-05-18 15:35:16 +0300

    Add outer sum approximation to report

--- source/index.m
+++ source/index.m
@@ -41,22 +41,29 @@ plot(xs, ys, 'r');
 plot(xs, ys, 'r');
 [xs, ys] = outer_ellipse_coords([1;2], q1Vec, q1Mat, q2Vec, q2Mat);
 plot(xs, ys, 'b');
-%%
+%% Рисуем внешнюю аппроксимацию суммы эллипсоидов
 q1Vec = [0; 0];
 q2Vec = [0; 0];
 q1Mat = [1, 0; 0, 2];
 q2Mat = [5, 3; 3, 2];
+
 figure, hold on, grid on;
-[xs, ys] = ellipse_coords(q1Vec, q1Mat);
-plot(xs, ys, 'r');
-[xs, ys] = ellipse_coords(q2Vec, q2Mat);
-plot(xs, ys, 'r');
+color1 = [0, 0.4470, 0.7410];
+color2 = [0.8500, 0.3250, 0.0980];
+
 phiVec = linspace(1, 2*pi, 100);
 for phi = phiVec
     lVec = [cos(phi); sin(phi)];
     [xs, ys] = outer_ellipse_coords(lVec, q1Vec, q1Mat, q2Vec, q2Mat);
-    plot(xs, ys, 'b');
+    p1 = plot(xs, ys, 'Color', color1);
 end
+
+[xs, ys] = sum_ellipse_coords(q1Vec, q1Mat, q2Vec, q2Mat);
+p2 = plot(xs, ys, 'Color', color2);
+
+xlabel('$$x_1$$', 'interpreter', 'latex');
+ylabel('$$x_2$$', 'interpreter', 'latex');
+legend([p2, p1], 'Сумма Минковского', 'Аппроксимации', 'Location', 'northwest');
 
 %%
 q1Vec = [0; 0];
